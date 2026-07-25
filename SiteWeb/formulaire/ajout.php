df0566a84ce9e924f18d3b96f8c6d19a97980ff9
Professeur Cot <martin.couture@uqtr.ca>
2026-07-25 16:41:30 -0400

    Corrections ajout.php
    Quelques ajustements, corrections et sécurités.

--- SiteWeb/formulaire/ajout.php
+++ SiteWeb/formulaire/ajout.php
@@ -1,14 +1,36 @@
 ﻿<!DOCTYPE html>
-
 <html>
 
 <head>
 	<title>Ajout des lieux d'intérêts</title>
 	<meta http-equiv="refresh" content="10; URL=https://professeurcot.net/tourisme/formulaire">
+	<meta name="Author" content="Martin Couture" /> 
+	<meta charset="utf-8" />  
 </head>
 
 <body>
 	<?php
+
+	//Récupération des données dans le fichier ini pour une meilleure sécurité. 
+	//Aide en ligne: https://sqlpey.com/php/securely-store-database-credentials-php/
+	$configPath = __DIR__ . '/../../fichiersconfigurationsPHP/db_config_tourisme.ini';
+	if (!file_exists($configPath)) {
+		die("Fichier de configuration pas trouvé.");
+	}
+
+	$dbSettings = parse_ini_file($configPath, true);
+	//Lecture du fichier de configuration
+	if (!$dbSettings) {
+		die("Impossible de lire le fichier de configuration.");
+	}
+
+	$dbHost = $dbSettings['database']['host'];
+	$dbName = $dbSettings['database']['dbname'];
+	$dbUser = $dbSettings['database']['username'];
+	$dbPass = $dbSettings['database']['password'];
+	$formPass = $dbSettings['database']['formpassword'];
+	//Fin de la récupération
+
 
 	$fNom = utf8_decode($_POST['fnom']);
 	$fCoordX = $_POST['fcoordx'];
@@ -17,32 +39,14 @@
 	$fInfo = utf8_decode($_POST['finfo']);
 	$motpasse = $_POST['fmotpasse'];
 
-	if ($motpasse == "teluq1430!") {
+	//Vérification du mot de passe du formulaire
+	if ($motpasse == $formPass) {
 
 
 		//Vérification du formulaire
 		if ($fNom == "" || $fCoordX == "" || $fCoordY == "" || $fUrl == "" || $fInfo == "") {
 				echo "<H2>Erreur</H2><H2>Formulaire incomplet!</H2>";
 			} else {
-
-				//Récupération des données dans le fichier ini pour une meilleure sécurité. 
-				//Aide en ligne: https://sqlpey.com/php/securely-store-database-credentials-php/
-				$configPath = __DIR__ . '/../../fichiersconfigurationsPHP/db_config_tourisme.ini';
-				if (!file_exists($configPath)) {
-					die("Fichier de configuration pas trouvé.");
-				}
-
-				$dbSettings = parse_ini_file($configPath, true);
-
-				if (!$dbSettings) {
-					die("Impossible de lire le fichier de configuration.");
-				}
-
-				$dbHost = $dbSettings['database']['host'];
-				$dbName = $dbSettings['database']['dbname'];
-				$dbUser = $dbSettings['database']['username'];
-				$dbPass = $dbSettings['database']['password'];
-
 
 				//Tentative de connexion
 				$mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);  // Connexion a MySQL
